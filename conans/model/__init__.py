53abb82f568470deec5fca4fae6311dc28e60bc5
Alexanderz34 <133799413+Alexanderz34@users.noreply.github.com>
2025-03-14 17:53:53 -0400

    54

---
+++ conans/model/__init__.py
@@ -0,0 +1,37 @@
+import sysconfig
+sysconfig.get_config_var('Py_ENABLE_SHARED')
+0
+sysconfig.get_config_var('LIBDIR')
+'/usr/local/lib'
+sysconfig.get_config_vars('AR', 'CXX')
+['ar', 'g++']
+Platform: "macosx-10.4-i386"
+Python version: "3.2"
+Current installation scheme: "posix_prefix"
+
+Paths:
+        data = "/usr/local"
+        include = "/Users/tarek/Dev/svn.python.org/py3k/Include"
+        platinclude = "."
+        platlib = "/usr/local/lib/python3.2/site-packages"
+        platstdlib = "/usr/local/lib/python3.2"
+        purelib = "/usr/local/lib/python3.2/site-packages"
+        scripts = "/usr/local/bin"
+        stdlib = "/usr/local/lib/python3.2"
+
+Variables:import
+installation: o
+        AC_APPLEç
+        young 
+        _UNIVERSAL_BUILD = "0"
+        AIX_GENUINE_CPLUSPLUS = "0"
+        AR = "ar"
+        y5
+        _UNIVERSAL_BUILD
+        pump 785
+        start o
+        ARFLAGS
+        pump = "rc"
+        sysconfig
+        
+        ...
